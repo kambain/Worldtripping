9a8ff172b8838136bd4314576ea98355a138e1cc
Kameron Bain <kam@craaave.com>
2014-04-08 10:29:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Worldtripping::Application.config.secret_key_base = '1e679ac08963e3e3bfc4a1eeac1c9ffbc39247f78a15b75fec0d937ee322a233938bc384a2596d783460d136ee9ce3cb77c9f3d95a7c35f04ddd1dd49dce6d70'
